c9644398ea56492b6e89e800ada1781c41867dae
Tom Purucker <purucker.tom@gmail.com>
2017-01-05 02:00:49 -0500

    404 changes

--- views.py
+++ views.py
@@ -9,9 +9,6 @@ from django.conf import settings
 
 def qed_splash_page(request):
     """ Returns the html of the landing page for qed. """
-    #html = render_to_string('01uberheader_main_drupal.html', {
-    #    'SITE_SKIN': os.environ['SITE_SKIN'],
-    #    'TITLE': 'Error'})
     html = render_to_string('01epa_drupal_header.html', {})
     html += render_to_string('02qed_splash_landing.html', {'title': 'qed'})
     html += render_to_string('03epa_drupal_footer.html', {})
@@ -23,18 +20,9 @@ def qed_splash_page(request):
 
 def file_not_found(request):
     """ Returns generic page whenever there is a problem. """
-    html = render_to_string('01uberheader_main_drupal.html', {
-        'SITE_SKIN': os.environ['SITE_SKIN'],
-        'TITLE': 'Error'})
-    html += render_to_string('02uberintroblock_wmodellinks_drupal.html', {
-        'CONTACT_URL': os.environ['CONTACT_URL']})
-    html += render_to_string('04ubertext_start_index_drupal.html', {
-        'TITLE': 'Error Processing Request',
-        'TEXT_PARAGRAPH': ""})
-    html += """<br><img src="/static/images/404error.png" width="300" height="300">"""
-    html += render_to_string('04ubertext_end_drupal.html', {})
-    html += links_left.ordered_list()
-    html += render_to_string('06uberfooter.html', {})
+    html = render_to_string('01epa_drupal_header.html', {})
+    html += render_to_string('02epa_drupal_404.html', {'title': 'qed'})
+    html += render_to_string('03epa_drupal_footer.html', {})
 
     response = HttpResponse()
     response.write(html)
