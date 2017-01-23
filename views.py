df2e415947caa448216d238c935ebe0364f87a45
Tom Purucker <purucker.tom@gmail.com>
2017-01-23 02:51:36 +0000

    shutter public site, link to qedinternal

--- views.py
+++ views.py
@@ -24,12 +24,16 @@ def qed_splash_page(request):
 
 
 def file_not_found(request):
-    """ Returns generic page whenever there is a problem. """
+    """ Returns the html of the landing page for qed. """
     html = render_to_string('01epa_drupal_header.html', {})
-    html += render_to_string('02epa_drupal_404.html', {'title': 'qed'})
+    html += render_to_string('02epa_drupal_header_bluestripe.html', {})
+    html += render_to_string('03epa_drupal_section_title.html', {})
+    if settings.IS_PUBLIC:
+        html += render_to_string('04qed_splash_landing_public.html', {'title': 'qed'})
+    else:
+        html += render_to_string('04qed_splash_landing_intranet.html', {'title': 'qed'})
+    html += render_to_string('09epa_drupal_splashscripts.html', {})
     html += render_to_string('10epa_drupal_footer.html', {})
-
     response = HttpResponse()
     response.write(html)
-
     return response
