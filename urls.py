f6802e640847c31f5240c3146646675aab08925a
Tom Purucker <purucker.tom@gmail.com>
2017-05-28 13:34:13 -0400

    changing splash page to links left

--- urls.py
+++ urls.py
@@ -2,13 +2,13 @@
 from django.conf import settings
 from django.conf.urls import url
 
-import views
+from views import landing
 
 if settings.IS_PUBLIC:
     urlpatterns = [
         #url(r'^api/', include('api.urls')),
         #url(r'^rest/', include('REST.urls')),
-        url(r'^$', views.qed_splash_page),
+        url(r'^$', landing.qed_splash_page_old),
         #url(r'^$', views.qed_splash_page_intranet),
         # url(r'^admin/', include(admin.site.urls)),
     ]
@@ -16,14 +16,14 @@ else:
     urlpatterns = [
         #url(r'^api/', include('api.urls')),
         #url(r'^rest/', include('REST.urls')),
-        url(r'^$', views.qed_splash_page),
+        url(r'^$', landing.splash_landing_page),
         #url(r'^$', views.qed_splash_page_intranet),
         # url(r'^admin/', include(admin.site.urls)),
     ]
 
 # 404 Error view (file not found)
-handler404 = views.file_not_found
+handler404 = landing.file_not_found
 # 500 Error view (server error)
-handler500 = views.file_not_found
+handler500 = landing.file_not_found
 # 403 Error view (forbidden)
-handler403 = views.file_not_found
+handler403 = landing.file_not_found
--- views/landing.py
+++ views/landing.py
@@ -3,9 +3,48 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+import links_left
+import os
 
+def splash_landing_page(request):
+    text_file2 = open(os.path.join(os.environ['PROJECT_PATH'], 'splash_app/views/landing_text.txt'), 'r')
+    xx = text_file2.read()
 
-def qed_splash_page(request):
+    #drupal template for header with bluestripe
+    #html = render_to_string('01epa_drupal_header.html', {})
+    html = render_to_string('01epa_drupal_header.html', {
+        'SITE_SKIN': os.environ['SITE_SKIN'],
+        'TITLE': u"\u00FCbertool"
+    })
+    #html = render_to_string('01uberheader_main_drupal.html', {
+    #    'SITE_SKIN': os.environ['SITE_SKIN'],
+    #    'TITLE': u"\u00FCbertool"
+    #})
+    html += render_to_string('02epa_drupal_header_bluestripe_onesidebar.html', {})
+    html += render_to_string('03epa_drupal_section_title_ubertool.html', {})
+
+    #main body of text
+    #html += render_to_string('04uber_drupal_frog_intro.html', {})
+    #http://jsfiddle.net/9zGQ8/
+
+    html += render_to_string('06ubertext_start_index_drupal.html', {
+        'TITLE': 'Ecological assessment of pesticides',
+        'TEXT_PARAGRAPH': xx
+    })
+    html += render_to_string('07ubertext_end_drupal.html', {})
+    html += links_left.ordered_list()  # fills out 05ubertext_links_left_drupal.html
+
+    #scripts and footer
+    html += render_to_string('09epa_drupal_ubertool_css.html', {})
+    #html += render_to_string('09epa_drupal_ubertool_scripts.html', {})
+    html += render_to_string('10epa_drupal_footer.html', {})
+
+    response = HttpResponse()
+    response.write(html)
+
+    return response
+
+def qed_splash_page_old(request):
     """ Returns the html of the landing page for qed. """
     html = render_to_string('01epa_drupal_header.html', {})
     html += render_to_string('02epa_drupal_header_bluestripe.html', {})
