85a4e6dbf6cd11c674c99275ab20bc7ef22c5f38
popenc <i.nickpope@gmail.com>
2017-12-05 16:22:11 -0500

    computer screen fatigue setting in..

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ if settings.IS_PUBLIC:
         #url(r'^api/', include('api.urls')),
         #url(r'^rest/', include('REST.urls')),
         # url(r'^$', landing.qed_splash_page_old),
-        url(r'^$', landing.qed_splash_page),
+        url(r'^$', landing.splash_landing_page),
         #url(r'^$', views.qed_splash_page_intranet),
         # url(r'^admin/', include(admin.site.urls)),
     ]
