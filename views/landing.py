99e93cdd2c5a554466e1f1ee8a317f00fa6f0fdb
Tom Purucker <purucker.tom@gmail.com>
2019-03-15 16:34:32 -0400

    adding html page that summarizes state of environmental variables

--- views/landing.py
+++ views/landing.py
@@ -62,6 +62,90 @@ def splash_landing_page(request):
 
     return response
 
+def whoami_table():
+    try:
+        html = "<br>"
+        html += "ENV_NAME = " + os.environ.get("ENV_NAME") + "<br>"
+        html += "CTS_EFS_SERVER = " + os.environ.get("CTS_EFS_SERVER") + "<br>"
+        html += "CTS_EPI_SERVER = " + os.environ.get("CTS_EPI_SERVER") + "<br>"
+        html += "CTS_JCHEM_SERVER = " + os.environ.get("CTS_JCHEM_SERVER") + "<br>"
+        html += "CTS_OPERA_SERVER = " + os.environ.get("CTS_OPERA_SERVER") + "<br>"
+        html += "CTS_REST_SERVER = " + os.environ.get("CTS_REST_SERVER") + "<br>"
+        html += "CTS_SPARC_SERVER = " + os.environ.get("CTS_SPARC_SERVER") + "<br>"
+        html += "CTS_TEST_SERVER = " + os.environ.get("CTS_TEST_SERVER") + "<br>"
+        html += "CYAN_REST_SERVER = " + os.environ.get("CYAN_REST_SERVER") + "<br>"
+        html += "DJANGO_SETTINGS_FILE = " + os.environ.get("DJANGO_SETTINGS_FILE") + "<br>"
+        html += "EPA_ACCESS_TEST_URL = " + os.environ.get("EPA_ACCESS_TEST_URL") + "<br>"
+        html += "HMS_BACKEND_SERVER = " + os.environ.get("HMS_BACKEND_SERVER") + "<br>"
+        html += "HMS_BACKEND_SERVER_DOCKER = " + os.environ.get("HMS_BACKEND_SERVER_DOCKER") + "<br>"
+        html += "HMS_BACKEND_SERVER_INTERNAL = " + os.environ.get("HMS_BACKEND_SERVER_INTERNAL") + "<br>"
+        html += "HMS_BACKEND_SERVER_URL = " + os.environ.get("HMS_BACKEND_SERVER_URL") + "<br>"
+        html += "HMS_LOCAL = " + os.environ.get("HMS_LOCAL") + "<br>"
+        html += "IN_DOCKER = " + os.environ.get("IN_DOCKER") + "<br>"
+        html += "IS_PUBLIC = " + os.environ.get("IS_PUBLIC") + "<br>"
+        html += "NODEJS_HOST = " + os.environ.get("NODEJS_HOST") + "<br>"
+        html += "NODEJS_PORT = " + os.environ.get("NODEJS_PORT") + "<br>"
+        html += "OPENCPU_REST_SERVER = " + os.environ.get("OPENCPU_REST_SERVER") + "<br>"
+        html += "REDIS_HOSTNAME = " + os.environ.get("REDIS_HOSTNAME") + "<br>"
+        html += "REDIS_PORT = " + os.environ.get("REDIS_PORT") + "<br>"
+        html += "<br>"
+    except:
+        html = "<br> Missing environmental variable! <br>"
+    return html
+
+def whoami_landing_page(request):
+    #text_file2 = open(os.path.join(os.environ['PROJECT_PATH'], 'splash_app/views/landing_text.txt'), 'r')
+    #xx = text_file2.read()
+    xx = get_html_text('landing_text_whoami.txt')
+    #drupal template for header with bluestripe
+    #templates_qed/drupal_2017
+    html = render_to_string('01epa_drupal_header.html', {
+        'SITE_SKIN': os.environ['SITE_SKIN'],
+        'TITLE': u"Q.E.D."
+    })
+    #html = render_to_string('01uberheader_main_drupal.html', {
+    #    'SITE_SKIN': os.environ['SITE_SKIN'],
+    #    'TITLE': u"\u00FCbertool"
+    #})
+    # templates_qed/drupal_2017
+    html += render_to_string('02epa_drupal_header_bluestripe_onesidebar.html', {})
+    # templates_qed/splash
+    html += render_to_string('03epa_drupal_section_title_splash.html', {})
+
+    #main body of text
+    #html += render_to_string('04uber_drupal_frog_intro.html', {})
+    #http://jsfiddle.net/9zGQ8/
+
+    yy = whoami_table()
+    zz = xx + yy
+
+    # templates_qed/uber2017
+    html += render_to_string('06ubertext_start_index_drupal.html', {
+        'TITLE': 'Environmental Variables',
+        'TEXT_PARAGRAPH': zz
+    })
+
+    # templates_qed/uber2017
+    html += render_to_string('07ubertext_end_drupal.html', {})
+
+    # fills out 05ubertext_links_left_drupal.html
+    if settings.IS_PUBLIC:
+        html += links_left.ordered_list_external()
+    else:
+        html += links_left.ordered_list_internal()
+
+    #scripts and footer
+    # templates_qed/uber2017
+    html += render_to_string('09epa_drupal_css.html', {})
+
+    # templates_qed/drupal_2017
+    html += render_to_string('10epa_drupal_footer.html', {})
+
+    response = HttpResponse()
+    response.write(html)
+
+    return response
+
 def wiki_landing_page(request):
     #text_file2 = open(os.path.join(os.environ['PROJECT_PATH'], 'splash_app/views/landing_text_wiki.txt'), 'r')
     #xx = text_file2.read()
